f8dfe55f5bf73e992ebdc6b85f5619a641948e21
unknown <ivanzatezalo@gmail.com>
2022-07-08 20:48:13 +0200

    Finished first 2 tasks

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from operator import contains
 import requests
 from decouple import config
 
-#get auth key from .env file
+# Get auth key from .env file
 auth_key = config("AUTH_KEY", default = None)
 
+# Wanted properties of each movie
+wanted_properties = "original_title", "genres", "release_date"
+
 def get_top_daily_movies():
-    """Fetches top movies on the first page for the current day"""
+    # Fetches top movies on the first page for the current day
 
     URL = f"https://api.themoviedb.org/3/movie/popular?api_key={auth_key}&language=en-US&page=1" 
     r= requests.get(url = URL)
@@ -14,21 +17,44 @@ def get_top_daily_movies():
 
 
 def get_movie_details(movie_id):
-    """ Get details of a movie bv id"""
+    # Get details of a movie bv id
 
     URL= f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={auth_key}&language=en-US"
     r= requests.get(url = URL)
-    return  r.json()
+    # Return only wanted properties, i.e.  "original_title", "genre", "release_date"
+    movie_details_specific = {key:value for key, value in r.json().items() if key in wanted_properties}
+    # Returm final dictionary containing both specific requests as the dictor and actors
+    return get_credits(movie_id, movie_details_specific)
+    
 
-def get_credits(movie_id):
-    """Get credits of a movie by id"""
+
+def get_credits(movie_id, movie_dictionary):
+    # Get credits of a movie by id
+
+    # Init empty list values for actors and directors
+    movie_dictionary["actors"] = []
+    movie_dictionary["directors"] = []
 
     URL = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key={auth_key}&language=en-US"
     r= requests.get(url = URL)
-    return  r.json()
+    data = r.json()
+
+    # Get directors and actors, NOTE - can be more efficient with list comprehension
+    
+    for key, value in data.items():
+        if key != "id":
+            for x in value:
+                if x["known_for_department"] == "Acting":
+                    movie_dictionary["actors"].append(x["name"])
+                elif "job" in x:
+                    if x["job"] == "Director":
+                        movie_dictionary["directors"].append(x["name"])
+     
+    # first value cointains actors the other list contains directors
+    return movie_dictionary
 
 
-wanted_properties= "original_title", "genre", "release_date"
+
 
 daily_movies_data = get_top_daily_movies()
 movie_results = daily_movies_data["results"]
@@ -36,9 +62,7 @@ movie_results = daily_movies_data["results"]
 
 extended_movies_results = {movie["id"]: get_movie_details(movie["id"]) for movie in movie_results}
 
-#print(extended_movies_results[453395])
-#extended_movies_results = [get_movie_details(movie["id"]) for movie in movie_results]
-movies_credits = [get_credits(movie["id"]) for movie in movie_results]
+print(extended_movies_results[453395])
 
-print(movies_credits[0])
+
 
