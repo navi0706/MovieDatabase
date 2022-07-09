8babe0031e56d2bc0e7a3f2d96000d1eca2c3388
unknown <ivanzatezalo@gmail.com>
2022-07-09 17:20:59 +0200

    added reviews

--- main.py
+++ main.py
@@ -9,14 +9,31 @@ auth_key = config("AUTH_KEY", default = None)
 # Wanted properties of each movie
 wanted_properties = "original_title", "genres", "release_date"
 
+#movie_id=453395
+def get_review(movie_id):
+    # Get user reviews of a movie, gets movie by movie_id
+
+    URL = f"https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key={auth_key}&language=en-US&page=1"
+    r= requests.get(url = URL)
+    data = r.json()
+
+    # Add reviews for each movie
+
+    # NOTE postprocessing can be done here, regex for reviews to remove URLs etc.
+    structued_reviews = [x["content"] for x in data["results"]]
+
+    return structued_reviews
+        
+
 def get_top_daily_movies():
     # Fetches top movies on the first page for the current day
 
     URL = f"https://api.themoviedb.org/3/movie/popular?api_key={auth_key}&language=en-US&page=1" 
     r= requests.get(url = URL)
-    return  r.json()
+    return r.json()
 
 
+        
 def get_movie_details(movie_id):
     # Get details of a movie bv id
 
@@ -24,6 +41,8 @@ def get_movie_details(movie_id):
     r= requests.get(url = URL)
     # Return only wanted properties, i.e.  "original_title", "genre", "release_date"
     movie_details_specific = {key:value for key, value in r.json().items() if key in wanted_properties}
+
+    movie_details_specific["reviews"] = get_review(movie_id)
     # Returm final dictionary containing both specific requests as well as the director of the movie and actors
     return get_credits(movie_id, movie_details_specific)
     
@@ -72,9 +91,12 @@ def save_as_json(data):
     with open("output.json", "w+", encoding="utf-8") as f:
         dump(data,f)
 
+#data=get_review(movie_id)
+#print(data)
 
 if __name__ == "__main__":
         data = structured_top_daily_movies()
         #save_as_json(data)
+        #print(data[615469])
 
 
